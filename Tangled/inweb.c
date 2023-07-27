4631afeb4d527191b6c26b91d8b1ecca1627d388
Graham Nelson <graham.nelson@mod-langs.ox.ac.uk>
2023-07-28 00:45:07 +0100

    Catching up

--- Tangled/inweb.c
+++ Tangled/inweb.c
@@ -9113,11 +9113,11 @@ int CommandLine__read_pair_p(text_stream *opt, text_stream *opt_val, int N,
 ; innocuous = TRUE; break;
 		case VERSION_CLSW: {
 			PRINT("inweb");
-			char *svn = "7.2.1-beta+1B27";
+			char *svn = "7.2.1-beta+1B28";
 			if (svn[0]) PRINT(" version %s", svn);
 			char *vname = "Escape to Danger";
 			if (vname[0]) PRINT(" '%s'", vname);
-			char *d = "6 July 2023";
+			char *d = "19 July 2023";
 			if (d[0]) PRINT(" (%s)", d);
 			PRINT("\n");
 			innocuous = TRUE; break;
@@ -33609,7 +33609,7 @@ void Ctags__write(web *W, filename *F) {
 	WRITE("!_TAG_FILE_SORTED\t0\t/0=unsorted, 1=sorted, 2=foldcase/\n");
 	WRITE("!_TAG_PROGRAM_AUTHOR\tGraham Nelson\t/graham.nelson@mod-langs.ox.ac.uk/\n");
 	WRITE("!_TAG_PROGRAM_NAME\tinweb\t//\n");
-	WRITE("!_TAG_PROGRAM_VERSION\t7.2.1-beta+1B27\t/built 6 July 2023/\n");
+	WRITE("!_TAG_PROGRAM_VERSION\t7.2.1-beta+1B28\t/built 19 July 2023/\n");
 
 }
 #line 47 "inweb/Chapter 6/Ctags Support.w"
